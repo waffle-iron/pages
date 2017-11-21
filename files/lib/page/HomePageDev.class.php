be9da932c56c62db54db51022f18e2c495607e78
Eric Dattore <edattore@gmail.com>
2017-11-20 18:11:56 -0700

    add development homepage php class

--- files/lib/page/HomePageDev.class.php
+++ files/lib/page/HomePageDev.class.php
@@ -4,9 +4,9 @@ namespace wcf\page;
 use wcf\data\user\group\UserGroupList;
 use wcf\system\WCF;
 
-class HomePage extends AbstractPage
+class HomePageDev extends AbstractPage
 {
-    public $activeMenuItem = 'wcf.page.home';
+    public $activeMenuItem = 'wcf.page.homedev';
 
     public function assignVariables()
     {
@@ -43,32 +43,48 @@ class HomePage extends AbstractPage
         $jsonPath = WCF_DIR . '../3rdparty/events.json';
         $json = json_decode(file_get_contents($jsonPath), true);
 
-        $defaults = ['icon' => 'fa-crosshairs', 'title' => '', 'description' => '', 'button' => '', 'url' => ''];
+        $defaults = ['icon' => 'fa-crosshairs', 'title' => '', 'summary' => '', 'button' => '', 'url' => ''];
         $entries = array_slice($json, 0, 4);
         foreach ($entries as &$entry) {
             $start = new \DateTime($entry['start']);
             $entry['date'] = strtoupper($start->format('dMY - g A') . ' EST');
 
-            $entry = array_merge($defaults, $entry); // Make sure it contains all array keys for the template to check
-
-            $description = $entry['description'];
-            $entry['description'] = '';
-	    $enabled = false;
-	    foreach (explode("\n", $description) as $line) {
-                if (strtolower($line) == 'generated_') {
-                    $enabled = true;
-                    continue;
-                }
-                if (!$enabled) continue;
-
-                $element = explode(':', $line, 2);
-                if (count($element) < 2) continue; // Sanity check
-
-                $entry[strtolower($element[0])] = trim($element[1]);
-            }
+            $entry = array_merge($defaults, $entry);
         }
 
         return $entries;
+
+        $events = [
+            'Navy' => ['date' => strtotime('this Sunday, 2pm')],
+            'Air Force' => ['date' => strtotime('this Thursday, 9pm')],
+            'Army' => ['date' => strtotime('this Wednesday, 9pm')],
+            'Marines' => ['date' => strtotime('this Saturday, 10pm')],
+        ];
+        uasort($events, function ($a, $b) { return ($a['date'] < $b['date']) ? -1 : 1; });
+
+        // Set defaults
+        foreach ($events as &$event) {
+            $time = $event['date'];
+            $event['date'] = strtoupper(date('dMY', $time)) . date(', gA', $time) . ' EST';
+            $event = array_merge($event, $defaults);
+        }
+
+        $lines = explode("\n", UNKSO_LANDING_UPCOMING);
+        $currentBranch = '';
+        foreach ($lines as $line) {
+            if (!strlen($line)) continue;
+
+            $split = explode(':', $line, 2);
+            if (count($split) == 1) {
+                $currentBranch = $line;
+            } else {
+                $split = array_map(function ($item) { return trim($item); }, $split);
+                $split[0] = strtolower($split[0]);
+                $events[$currentBranch][$split[0]] = $split[1];
+            }
+        }
+
+        return $events;
     }
 
     protected function getBranchNumbers()
