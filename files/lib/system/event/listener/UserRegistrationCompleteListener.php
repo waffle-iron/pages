71179f3872d0dad2342606d285336deba398f743
MichaelJamesParsons <mjay.parsons@gmail.com>
2018-01-15 23:48:27 -0500

    Added event listener for registration.

---
+++ files/lib/system/event/listener/UserRegistrationCompleteListener.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace wcf\system\event\listener;
+
+use wcf\system\WCF;
+
+/**
+ * Class UserRegistrationCompleteListener
+ * @package wcf\system\event\listener
+ */
+class UserRegistrationCompleteListener implements IParameterizedEventListener
+{
+    public function execute($eventObj, $className, $eventName, array &$parameters) {
+
+    }
+}
