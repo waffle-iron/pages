8399f6fbe369385b352d943248053e9d6674ce96
MichaelJamesParsons <mjay.parsons@gmail.com>
2018-01-16 20:28:27 -0500

    Added logger to user registration event listener

--- files/lib/system/event/listener/UserRegistrationCompleteListener.php
+++ files/lib/system/event/listener/UserRegistrationCompleteListener.php
@@ -11,6 +11,18 @@ use wcf\system\WCF;
 class UserRegistrationCompleteListener implements IParameterizedEventListener
 {
     public function execute($eventObj, $className, $eventName, array &$parameters) {
+        $this->logEvents($eventObj, $className, $eventName, $parameters);
+    }
 
+    private function logEvents ($eventObj, $className, $eventName, array &$parameters) {
+        $time = time();
+        $content = "\n\n{$time} -------------------------------------------------------------";
+        $content .= "\nclassName: {$className}";
+        $content .= "\neventName: {$eventName}";
+        $content .= "\nevent object:\n";
+        $content .= json_encode($eventObj);
+        $content .= "\nparams: " . implode(',', $parameters);
+
+        file_put_contents('./events.log', $content, FILE_APPEND | LOCK_EX);
     }
 }
